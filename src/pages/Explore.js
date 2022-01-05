696868b98c5bb4343dd0ced8e74f267e9d137892
Rodolfo <rodolfoserralha@hotmail.com>
2022-01-05 16:01:01 -0300

    adicionando requisitos 67-68-69

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Explore() {
+  const history = useHistory();
+
   return (
     <>
       <Header title="Explorar" />
-      <div>
-        Explorar
+      <div id="explore-btns">
+        <button
+          data-testid="explore-food"
+          className="explore-btn"
+          type="button"
+          onClick={ () => history.push('/explorar/comidas') }
+        >
+          Explorar Comidas
+        </button>
+
+        <button
+          data-testid="explore-drinks"
+          className="explore-btn"
+          type="button"
+          onClick={ () => history.push('/explorar/bebidas') }
+        >
+          Explorar Bebidas
+        </button>
       </div>
       <Footer />
     </>
