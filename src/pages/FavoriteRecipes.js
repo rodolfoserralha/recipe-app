12abbf60571fedc2a6ac49fa02c434e115b18d04
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-01-06 20:05:33 -0300

    fix erros

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,50 +1,50 @@
-import React, { useState } from 'react';
-import Header from '../components/Header';
+// import React, { useState } from 'react';
+// import Header from '../components/Header';
 
-export default function FavoriteRecipes() {
-  const favoritesArray = JSON.parse(localStorage.getItem('favoriteRecipes'));
-  const [favorites, setFavorites] = useState(favoritesArray);
+// export default function FavoriteRecipes() {
+//   const favoritesArray = JSON.parse(localStorage.getItem('favoriteRecipes'));
+//   const [favorites, setFavorites] = useState(favoritesArray);
 
-  function showAll() {
-    setFavorites(favoritesArray);
-  }
+//   function showAll() {
+//     setFavorites(favoritesArray);
+//   }
 
-  function justFood() {
-    const justFoodArray = favoritesArray.filter((favorite) => favorite.type === 'comida');
-    setFavorites(justFoodArray);
-  }
-  function justDrinks() {
-    const justDrinksArray = favoritesArray
-      .filter((favorite) => favorite.type === 'bebidas');
-    setFavorites(justDrinksArray);
-  }
+//   function justFood() {
+//     const justFoodArray = favoritesArray.filter((favorite) => favorite.type === 'comida');
+//     setFavorites(justFoodArray);
+//   }
+//   function justDrinks() {
+//     const justDrinksArray = favoritesArray
+//       .filter((favorite) => favorite.type === 'bebidas');
+//     setFavorites(justDrinksArray);
+//   }
 
-  return (
-    <>
-      <Header title="Receitas Favoritas" />
-      <div>
-        <button
-          type="button"
-          data-testid="filter-by-all-btn"
-          onClick={ showAll }
-        >
-          All
-        </button>
-        <button
-          type="button"
-          data-testid="filter-by-food-btn"
-          onClick={ justFood }
-        >
-          Food
-        </button>
-        <button
-          type="button"
-          data-testid="filter-by-drink-btn"
-          onClick={ justDrinks }
-        >
-          Drinks
-        </button>
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Header title="Receitas Favoritas" />
+//       <div>
+//         <button
+//           type="button"
+//           data-testid="filter-by-all-btn"
+//           onClick={ showAll }
+//         >
+//           All
+//         </button>
+//         <button
+//           type="button"
+//           data-testid="filter-by-food-btn"
+//           onClick={ justFood }
+//         >
+//           Food
+//         </button>
+//         <button
+//           type="button"
+//           data-testid="filter-by-drink-btn"
+//           onClick={ justDrinks }
+//         >
+//           Drinks
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
