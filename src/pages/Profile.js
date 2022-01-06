fbc52c4898cd6a6284a3a44c34cc513a1c347ded
Rodolfo <rodolfoserralha@hotmail.com>
2022-01-06 16:48:48 -0300

    adiciona requisitos 82 a 87

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const { email } = (JSON.parse(localStorage.getItem('user')));
+  const history = useHistory();
+
+  function handleLogout() {
+    history.push('/');
+    localStorage.clear();
+  }
+
   return (
     <>
       <Header title="Perfil" />
-      <div>
-        Profile
+      <div className="parent-cards">
+        <h2 data-testid="profile-email">{ email }</h2>
+
+        <button
+          className="explore-btn"
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+
+        <button
+          data-testid="profile-favorite-btn"
+          className="explore-btn"
+          type="button"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+
+        <button
+          data-testid="profile-logout-btn"
+          className="explore-btn"
+          type="button"
+          onClick={ handleLogout }
+        >
+          Sair
+        </button>
       </div>
       <Footer />
     </>
