09956b03655545fe58faaa2561bdc62a30264d95
Marco <marquinco@outlook.com>
2022-01-08 17:53:12 -0300

    Une implementaçao da correçao do 11 com 83

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import LoginContext from '../context/LoginContext';
 
 export default function Profile() {
+  const [emailLocal, setEmailLocal] = useState('');
+  useEffect(() => {
+    const localStorageItem = JSON.parse(localStorage.getItem('user'));
+    if (localStorageItem) {
+      const { email: emailFromLocal } = localStorageItem;
+      setEmailLocal(emailFromLocal);
+    }
+  }, []);
   const { user: { email } } = useContext(LoginContext);
-  const { email: emailLocal } = (JSON.parse(localStorage.getItem('user')));
+
   const history = useHistory();
 
   function handleLogout() {
